fa658f7db205bd925ad3c2d17011617bd9f58ef3
Soheil Novinfard <novinfard@gmail.com>
2020-02-03 22:47:27 +0000

    Initial Commit

---
+++ NewsReader/NewsReader.h
@@ -0,0 +1,19 @@
+//
+//  NewsReader.h
+//  NewsReader
+//
+//  Created by Soheil on 03/02/2020.
+//  Copyright © 2020 Novinfard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NewsReader.
+FOUNDATION_EXPORT double NewsReaderVersionNumber;
+
+//! Project version string for NewsReader.
+FOUNDATION_EXPORT const unsigned char NewsReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewsReader/PublicHeader.h>
+
+
