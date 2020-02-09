0043d846e291f707571ddc8ae81286013379cc72
Soheil Novinfard <novinfard@gmail.com>
2020-02-09 17:47:44 +0000

    Move mapping(parsing) logic to a new type `NewstemsMapper`

--- NewsReader/Reader API/RemoteNewsReader.swift
+++ NewsReader/Reader API/RemoteNewsReader.swift
@@ -43,18 +43,10 @@ public final class RemoteNewsReader {
 		client.get(from: url, completion: { result in
 			switch result {
 			case let .success(data, response):
-				if response.statusCode == 200 {
-					do {
-						let jsonDecoder = JSONDecoder()
-						jsonDecoder.dateDecodingStrategy = .formatted(DateFormatter.iso8601)
-						let response = try jsonDecoder.decode(Response.self, from: data)
-
-						return completion(.success(response.news.map({$0.newsItem})))
-					} catch {
-						print(error)
-						return completion(.failure(.invalidData))
-					}
-				} else {
+				do {
+					let newsItems = try NewsItemsMapper.map(data, response)
+					return completion(.success(newsItems))
+				} catch {
 					return completion(.failure(.invalidData))
 				}
 			case .failure:
@@ -65,69 +57,89 @@ public final class RemoteNewsReader {
 
 }
 
-private struct Response: Decodable, Equatable {
-	let status: String
-	let totalResults: Int
-	let news: [ApiNewsItem]
+private class NewsItemsMapper {
 
-	init(status: String,
-				totalResults: Int,
-				news: [ApiNewsItem]) {
+	private struct Response: Decodable, Equatable {
+		let status: String
+		let totalResults: Int
+		let news: [ApiNewsItem]
 
-		self.status = status
-		self.totalResults = totalResults
-		self.news = news
-	}
-}
+		init(status: String,
+					totalResults: Int,
+					news: [ApiNewsItem]) {
 
-private struct ApiNewsItem: Decodable, Equatable {
-	let id: UUID
-	let source: ApiSource
-	let tags: [ApiTag]?
-	let author: String
-	let title: String
-	let description: String
-	let urlToImage: URL?
-	let content: String
-
-	var newsItem: NewsItem {
-		return NewsItem(
-			id: id,
-			source: source.source,
-			tags: tags?.map({$0.tag}),
-			author: author,
-			title: title,
-			description: description,
-			urlToImage: urlToImage,
-			content: content
-		)
-	}
-}
-
-private struct ApiSource: Decodable, Equatable {
-	let id: UUID?
-	let name: String
-
-	init(id: UUID?, name: String) {
-		self.id = id
-		self.name = name
+			self.status = status
+			self.totalResults = totalResults
+			self.news = news
+		}
 	}
 
-	var source: Source {
-		return Source(id: id, name: name)
+	private struct ApiNewsItem: Decodable, Equatable {
+		let id: UUID
+		let source: ApiSource
+		let tags: [ApiTag]?
+		let author: String
+		let title: String
+		let description: String
+		let urlToImage: URL?
+		let content: String
+
+		var newsItem: NewsItem {
+			return NewsItem(
+				id: id,
+				source: source.source,
+				tags: tags?.map({$0.tag}),
+				author: author,
+				title: title,
+				description: description,
+				urlToImage: urlToImage,
+				content: content
+			)
+		}
 	}
-}
 
-private struct ApiTag: Decodable, Equatable {
-	let id: UUID
-	let name: String
+	private struct ApiSource: Decodable, Equatable {
+		let id: UUID?
+		let name: String
 
-	init(id: UUID, name: String) {
-		self.id = id
-		self.name = name
+		init(id: UUID?, name: String) {
+			self.id = id
+			self.name = name
+		}
+
+		var source: Source {
+			return Source(id: id, name: name)
+		}
 	}
 
-	var tag: Tag {
-		return Tag(id: id, name: name)
+	private struct ApiTag: Decodable, Equatable {
+		let id: UUID
+		let name: String
+
+		init(id: UUID, name: String) {
+			self.id = id
+			self.name = name
+		}
+
+		var tag: Tag {
+			return Tag(id: id, name: name)
+		}
+	}
+
+	class func map(_ data: Data, _ response: HTTPURLResponse) throws -> [NewsItem] {
+		guard response.statusCode == 200 else {
+			throw RemoteNewsReader.Error.invalidData
+		}
+
+		do {
+			let jsonDecoder = JSONDecoder()
+			jsonDecoder.dateDecodingStrategy = .formatted(DateFormatter.iso8601)
+			let response = try jsonDecoder.decode(Response.self, from: data)
+
+			return response.news.map({$0.newsItem})
+		} catch {
+			print(error)
+			throw RemoteNewsReader.Error.invalidData
+		}
 	}
 }
