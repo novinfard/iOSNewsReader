6780ce058c319204631323168f65ec5487aab72e
Soheil Novinfard <novinfard@gmail.com>
2020-02-10 23:57:53 +0000

    Add nil data and nil response to the request error test case to make the setup more explicit

--- NewsReaderTests/Reader API/URLSessionHttpClientTests.swift
+++ NewsReaderTests/Reader API/URLSessionHttpClientTests.swift
@@ -32,7 +32,7 @@ class URLSessionHttpClientTests: XCTestCase {
 
 		let url = URL(string: "https://a-url.com")!
 		let error = NSError(domain: "Domain error", code: 1)
-		URLProtocolStub.stub(url: url, error: error)
+		URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
 
 		let sut = URLSessionHttpClient()
 
@@ -58,11 +58,13 @@ class URLSessionHttpClientTests: XCTestCase {
 		private static var stubs = [URL: Stub]()
 
 		private struct Stub {
+			let data: Data?
+			let response: URLResponse?
 			let error: Error?
 		}
 
-		static func stub(url: URL, error: Error? = nil) {
-			stubs[url] = Stub(error: error)
+		static func stub(url: URL, data: Data?, response: URLResponse?,  error: Error?) {
+			stubs[url] = Stub(data: data, response: response, error: error)
 		}
 
 		static func startInterceptingRequest() {
@@ -92,6 +94,14 @@ class URLSessionHttpClientTests: XCTestCase {
 
 			if let error = stub.error {
 				client?.urlProtocol(self, didFailWithError: error)
+			}
+
+			if let data = stub.data {
+				client?.urlProtocol(self, didLoad: data)
+			}
+
+			if let response = stub.response {
+				client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
 			}
 
 			client?.urlProtocolDidFinishLoading(self)
