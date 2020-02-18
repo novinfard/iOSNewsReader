9b1fcf8e792ba3750ec15b640e33055977502fc4
Soheil Novinfard <novinfard@gmail.com>
2020-02-18 23:15:39 +0000

    Save command requests cache deletion

--- NewsReaderTests/News Cache/CacheNewsUseCaseTests.swift
+++ NewsReaderTests/News Cache/CacheNewsUseCaseTests.swift
@@ -7,15 +7,25 @@
 //
 
 import XCTest
+import NewsReader
 
 class LocalNewsReader {
+	private let store: NewsStore
 	init(store: NewsStore) {
+		self.store = store
+	}
 
+	func save(_ items: [NewsItem]) {
+		store.deleteCachedNews()
 	}
 }
 
 class NewsStore {
 	var deleteCachedNewsCallCount = 0
+
+	func deleteCachedNews() {
+		deleteCachedNewsCallCount += 1
+	}
 }
 
 class CacheNewsUseCaseTests: XCTestCase {
@@ -27,4 +37,30 @@ class CacheNewsUseCaseTests: XCTestCase {
 		XCTAssertEqual(store.deleteCachedNewsCallCount, 0)
 	}
 
+	func test_save_requestsCacheDeletion() {
+		let store = NewsStore()
+		let sut = LocalNewsReader(store: store)
+		let items = [uniqueItem(), uniqueItem()]
+		sut.save(items)
+
+		XCTAssertEqual(store.deleteCachedNewsCallCount, 1)
+	}
+
+	// MARK: - Helpers
+	func uniqueItem() -> NewsItem {
+		return NewsItem(
+			id: anyId(),
+			source: Source(id: nil, name: "any"),
+			tags: nil,
+			author: "any",
+			title: "any",
+			description: "any",
+			urlToImage: URL(string: "https://a-url")!,
+			content: "any"
+		)
+	}
+
+	private func anyId() -> Int {
+		return Int.random(in: 1 ... 1_000_000)
+	}
 }
