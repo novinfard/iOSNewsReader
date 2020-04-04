97d94221d825cbbe139dec8ac8919af4b844e0fd
Soheil Novinfard <novinfard@gmail.com>
2020-04-04 01:00:39 +0100

    Guarantee that `LocalNewsReader` does not deliver insertion error after instance has been deallocated

--- NewsReaderTests/News Cache/CacheNewsUseCaseTests.swift
+++ NewsReaderTests/News Cache/CacheNewsUseCaseTests.swift
@@ -21,7 +21,10 @@ class LocalNewsReader {
 		store.deleteCachedNews() { [weak self] error in
 			guard let self = self else { return }
 			if error == nil {
-				self.store.insert(items, timestamp: self.currentDate(), completion: completion)
+				self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
+					guard self != nil else { return }
+					completion(error)
+				}
 			} else {
 				completion(error)
 			}
@@ -114,6 +117,20 @@ class CacheNewsUseCaseTests: XCTestCase {
 
 		sut = nil
 		store.completeDeletion(with: anyNSError())
+
+		XCTAssertTrue(receivedResults.isEmpty)
+	}
+
+	func test_save_doesNotDeliverInsertionErrorAfterSUTInstanceHasBeenDeallocated() {
+		let store = NewsStoreSpy()
+		var sut: LocalNewsReader? = LocalNewsReader(store: store, currentDate: Date.init)
+
+		var receivedResults = [Error?]()
+		sut?.save([uniqueItem()]) { receivedResults.append($0) }
+
+		store.completeDeletionSuccessfully()
+		sut = nil
+		store.completeInsertion(with: anyNSError())
 
 		XCTAssertTrue(receivedResults.isEmpty)
 	}
