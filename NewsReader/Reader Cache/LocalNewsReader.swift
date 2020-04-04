1fc6a88749c098a1d7201195b6f450c001879aba
Soheil Novinfard <novinfard@gmail.com>
2020-04-04 01:15:32 +0100

    Move `LocalNewsReader` (and `NewsStore` collaborator ) to its own file in production

---
+++ NewsReader/Reader Cache/LocalNewsReader.swift
@@ -0,0 +1,44 @@
+//
+//  LocalNewsReader.swift
+//  NewsReader
+//
+//  Created by Soheil on 04/04/2020.
+//  Copyright © 2020 Novinfard. All rights reserved.
+//
+
+import Foundation
+
+public final class LocalNewsReader {
+	private let store: NewsStore
+	private let currentDate: () -> Date
+	public init(store: NewsStore, currentDate: @escaping () -> Date) {
+		self.store = store
+		self.currentDate = currentDate
+	}
+
+	public func save(_ items: [NewsItem], completion: @escaping (Error?) -> Void) {
+		store.deleteCachedNews() { [weak self] error in
+			guard let self = self else { return }
+			if let cacheDeletionError = error {
+				completion(cacheDeletionError)
+			} else {
+				self.cache(items, with: completion)
+			}
+		}
+	}
+
+	private func cache(_ items: [NewsItem], with completion: @escaping (Error?) -> Void) {
+		store.insert(items, timestamp: currentDate()) { [weak self] error in
+			guard self != nil else { return }
+			completion(error)
+		}
+	}
+}
+
+public protocol NewsStore {
+	typealias DeletionCompletion = (Error?) -> Void
+	typealias InsertionCompletion = (Error?) -> Void
+
+	func deleteCachedNews(completion: @escaping DeletionCompletion)
+	func insert(_ items: [NewsItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
