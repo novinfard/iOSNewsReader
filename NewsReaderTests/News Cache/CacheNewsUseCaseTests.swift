f2d3eb234128f098c9f72df66d9cfe15a4d4fade
Soheil Novinfard <novinfard@gmail.com>
2020-04-04 01:10:44 +0100

    Extract cache insertion into a helper function to make logic inside closure callbacks easier to follow

--- NewsReaderTests/News Cache/CacheNewsUseCaseTests.swift
+++ NewsReaderTests/News Cache/CacheNewsUseCaseTests.swift
@@ -23,11 +23,15 @@ class LocalNewsReader {
 			if let cacheDeletionError = error {
 				completion(cacheDeletionError)
 			} else {
-				self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
-					guard self != nil else { return }
-					completion(error)
-				}
+				self.cache(items, with: completion)
 			}
+		}
+	}
+
+	private func cache(_ items: [NewsItem], with completion: @escaping (Error?) -> Void) {
+		store.insert(items, timestamp: currentDate()) { [weak self] error in
+			guard self != nil else { return }
+			completion(error)
 		}
 	}
 }
