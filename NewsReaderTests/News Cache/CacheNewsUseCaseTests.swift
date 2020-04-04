4625ede1787ad304fb9a8f7cc631bb63f6d008bd
Soheil Novinfard <novinfard@gmail.com>
2020-04-04 01:05:22 +0100

    Invert If logic to to make the code path easier to follow

--- NewsReaderTests/News Cache/CacheNewsUseCaseTests.swift
+++ NewsReaderTests/News Cache/CacheNewsUseCaseTests.swift
@@ -20,13 +20,13 @@ class LocalNewsReader {
 	func save(_ items: [NewsItem], completion: @escaping (Error?) -> Void) {
 		store.deleteCachedNews() { [weak self] error in
 			guard let self = self else { return }
-			if error == nil {
+			if let cacheDeletionError = error {
+				completion(cacheDeletionError)
+			} else {
 				self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
 					guard self != nil else { return }
 					completion(error)
 				}
-			} else {
-				completion(error)
 			}
 		}
 	}
