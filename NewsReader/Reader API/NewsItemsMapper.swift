8b62f4c61c49d1fb2cb5c591ea41294ce747db2f
Soheil Novinfard <novinfard@gmail.com>
2020-04-05 23:19:26 +0100

    Move `RemoteNewsItem` to its own file

--- NewsReader/Reader API/NewsItemsMapper.swift
+++ NewsReader/Reader API/NewsItemsMapper.swift
@@ -8,27 +8,6 @@
 
 import Foundation
 
-internal struct RemoteNewsItem: Decodable, Equatable {
-	let id: Int
-	let source: RemoteSource
-	let tags: [RemoteTag]?
-	let author: String
-	let title: String
-	let description: String
-	let urlToImage: URL?
-	let content: String
-}
-
-internal struct RemoteSource: Decodable, Equatable {
-	let id: Int?
-	let name: String
-}
-
-internal struct RemoteTag: Decodable, Equatable {
-	let id: Int
-	let name: String
-}
-
 internal final class NewsItemsMapper {
 
 	private struct Response: Decodable, Equatable {
