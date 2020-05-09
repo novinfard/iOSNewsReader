e798d7b65d472a02298ecaa5d7e93529cc94006f
Soheil Novinfard <novinfard@gmail.com>
2020-05-09 19:37:38 +0100

    Add comments to document that completion handlers in any `NewsStore` implementation can be invoked in any thread. Clients are responsible to dispatch to appropriate threads, if needed.

--- NewsReader/Reader Cache/NewsStore.swift
+++ NewsReader/Reader Cache/NewsStore.swift
@@ -19,7 +19,15 @@ public protocol NewsStore {
 	typealias InsertionCompletion = (Error?) -> Void
 	typealias RetrievalCompletion = (RetrievedCachedNewsResult) -> Void
 
+	/// The completion handler can be invoked in any thread.
+ 	/// Clients are responsible to dispatch to appropriate threads, if needed.
 	func deleteCachedNews(completion: @escaping DeletionCompletion)
+
+	/// The completion handler can be invoked in any thread.
+ 	/// Clients are responsible to dispatch to appropriate threads, if needed.
 	func insert(_ items: [LocalNewsItem], timestamp: Date, completion: @escaping InsertionCompletion)
+
+	/// The completion handler can be invoked in any thread.
+ 	/// Clients are responsible to dispatch to appropriate threads, if needed.
 	func retrieve(completion: @escaping RetrievalCompletion)
 }
