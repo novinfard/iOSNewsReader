ea7ab01d951d301711630dba3a0ad3c4f4a1ae50
Soheil Novinfard <novinfard@gmail.com>
2020-05-09 11:45:51 +0100

    Extract duplicate delete test code into a reusable helper method

--- NewsReaderTests/News Cache/CodableNewsStoreTests.swift
+++ NewsReaderTests/News Cache/CodableNewsStoreTests.swift
@@ -221,13 +221,9 @@ class CodableNewsStoreTests: XCTestCase {
 
 	func test_delete_hasNoSideEffectsOnEmptyCache() {
  		let sut = makeSUT()
- 		let exp = expectation(description: "Wait for cache deletion")
+		let deletionError = deleteCache(from: sut)
 
- 		sut.deleteCachedFeed { deletionError in
- 			XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
- 			exp.fulfill()
- 		}
- 		wait(for: [exp], timeout: 1.0)
+ 		XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
 
  		expect(sut, toRetrieve: .empty)
  	}
@@ -236,12 +232,9 @@ class CodableNewsStoreTests: XCTestCase {
  		let sut = makeSUT()
  		insert((uniqueItems().local, Date()), to: sut)
 
- 		let exp = expectation(description: "Wait for cache deletion")
- 		sut.deleteCachedFeed { deletionError in
- 			XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
- 			exp.fulfill()
- 		}
- 		wait(for: [exp], timeout: 1.0)
+		let deletionError = deleteCache(from: sut)
+
+ 		XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
 
  		expect(sut, toRetrieve: .empty)
  	}
@@ -272,6 +265,17 @@ class CodableNewsStoreTests: XCTestCase {
  		wait(for: [exp], timeout: 1.0)
 
 		return insertionError
+ 	}
+
+	private func deleteCache(from sut: CodableNewsStore) -> Error? {
+ 		let exp = expectation(description: "Wait for cache deletion")
+ 		var deletionError: Error?
+ 		sut.deleteCachedFeed { receivedDeletionError in
+ 			deletionError = receivedDeletionError
+ 			exp.fulfill()
+ 		}
+ 		wait(for: [exp], timeout: 1.0)
+ 		return deletionError
  	}
 
 	private func expect(_ sut: CodableNewsStore,
